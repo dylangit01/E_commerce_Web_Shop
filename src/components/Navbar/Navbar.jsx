d60a3c2f9debf90181dd4d60dc5bf7cae88b62a1
Dylan <dcren2010@gmail.com>
2021-08-07 13:50:04 -0400

    create navbar and update styles

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Typography, AppBar, Toolbar, IconButton, Badge, MenuItem, Menu } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import useStyles from './styles';
+import eShopLogo from '../../assets/online-shopping-logo-2.png'
+
+const Navbar = () => {
+	const classes = useStyles();
+	return (
+		<>
+			<AppBar position='fixed' className={classes.appBar} color='inherit'>
+				<Toolbar>
+					<Typography className={classes.logoImg}>
+						<img src={eShopLogo} alt='DylanCommerceShop.js' height='35px' className={classes.image} />
+						Online Web Shop
+					</Typography>
+					<div className={classes.grow} />
+					<div className={classes.button}>
+						<IconButton aria-label='Show cart items' color='inherit'>
+							<Badge badgeContent={3} color='secondary'>
+								<ShoppingCart />
+							</Badge>
+						</IconButton>
+					</div>
+				</Toolbar>
+			</AppBar>
+		</>
+	);
+}
+
+export default Navbar
