e4cc7b6100e337929b39225e2caf0fb21df70dfe
Dylan <dcren2010@gmail.com>
2021-08-08 09:46:19 -0400

    fix hook-form error

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { useFormContext, Controller } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { TextField, Grid } from '@material-ui/core';
 
 function FormInput({ name, label, required }) {
-	const { control } = useFormContext();
+	const { control } = useForm();
 	const isError = false;
 
 	return (
 		<Grid item xs={12} sm={6}>
 			<Controller
 				control={control}
-				render={({ field }) => <TextField {...field} label={label} />}
+				render={({ field }) => <TextField fullWidth {...field} label={label} required={required} />}
 				name={name}
-				fullWidth
-				required={required}
 				error={isError}
 			/>
 		</Grid>
